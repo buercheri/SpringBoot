28ac03650183efccddf3caa4ced2d8023fc07559
buercheri <buercheri@gmail.com>
2019-02-14 22:48:17 +0100

    Swagger configuration

---
+++ README.md
@@ -0,0 +1,10 @@
+Spring Boot
+Experimentation of Spring Boot for Java
+
+Configuration by using application.properties
+http://localhost:12378/demo/api/v1/hello
+
+Swagger
+http://localhost:12378/demo/swagger-ui.html
+- see https://dzone.com/articles/spring-boot-2-restful-api-documentation-with-swagg
+- see https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 @RestController
+@RequestMapping("/api/v1")
+@Api(value="HelloController")
 public class HelloController {
 
-    @RequestMapping("/hello")
+	@ApiOperation(value = "Hello world", response = String.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Successfully retrieved message"),
+	    @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
+	@GetMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
